fea12b76c93d3870d31fc5536e4538262473e872
JulianDataAnalytics <julianlop97@gmail.com>
2026-02-23 12:02:29 -0500

    Day 1 - Personal profile calculator

---
+++ day1_perfil_personal.py
@@ -0,0 +1,25 @@
+# EJERCICIOS DE PRATICA
+
+name = input(f" enter  your name: ")
+edad = int(input(f" enter your age: "))  
+ciudad = input(f" enter your city: ")   
+altura = float(input(f" enter your height in meters:  "))
+peso = float(input(f" enter your weight in kilograms:  "))
+IMC = peso / (altura ** 2)
+
+
+print(f" hello {name} ")
+print(f" actualmente vives en {ciudad} ")
+print(f" tu edad es {edad} años y en 5 años tendras {edad + 5} años ")
+print(f" segun tu edad actual naciste en el año {2026 - edad} ")
+print(f" tu IMC ES DE : {peso} / ({altura ** 2}) = {IMC}")
+
+
+if IMC < 18.5:
+    print("BAJO PESO")
+if IMC >= 18.5 and IMC < 24.9:
+    print("PESO NORMAL")
+if IMC > 25: 
+    print("SOBREPESO")
+
+
